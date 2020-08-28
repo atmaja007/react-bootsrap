ade427f1148875ed7dadd862140ac48a813d91b1
atmaja007 <ulatbulubibi@gmail.com>
2020-08-28 16:46:29 +0700

    Buat baru commit 1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Home from './pages/Home';
+import About from './pages/About';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const RootApp = () =>(
+  <BrowserRouter>
+      <Switch>
+         <Route exact path="/" component={App}/>
+         <Route path="/home" component={Home}/>
+         <Route path="/About" component={About}/>
+      </Switch>
+  </BrowserRouter>
+)
+
+// ReactDOM.render(<Hello username={"Mustajib"} />, document.getElementById('root'));
+ReactDOM.render(<RootApp />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const About = () => {
+    return ( <div>
+        <Navbar />
+        <h1>Ini Halaman About</h1>
+    </div> );
+}
+ 
+export default About;
